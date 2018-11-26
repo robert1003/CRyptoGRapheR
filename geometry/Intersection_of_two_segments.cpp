11226ffb48471e199a853ac12a0af13716075298
Giver <ryan01234keroro56789@yahoo.com.tw>
2018-11-26 22:37:03 +0800

    Update Intersection_of_two_segments.cpp

--- geometry/Intersection_of_two_segments.cpp
+++ geometry/Intersection_of_two_segments.cpp
@@ -1,10 +1,10 @@
-bool onseg(Pt p, Line L) {
+bool onseg(Pt p, Line L) { //assume dcmp(x^y)==0
   Pt x = L.s-p, y = L.e-p;
-  return dcmp(x^y) == 0 && dcmp(x*y) <= 0; // inseg: dcmp(x*y) < 0
+  return dcmp(x*y) <= 0; //inseg:dcmp(x*y)<0
 }
 Pt SSIntersect(Line a, Line b) {
   Pt p1 = a.s, p2 = a.e, q1 = b.s, q2 = b.e;
   Pt p = LLIntersect(a, b);
-  if(!isnan(p.x) && onseg(p, a) && onseg(p, b)) return p;
+  if(!isnan(p.x)&&onseg(p, a)&&onseg(p, b)) return p;
   return {nan(""), nan("")};
 }
