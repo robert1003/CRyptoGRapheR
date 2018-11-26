ab5bbef6582d5b7457270e4936e02e6c4b33bcd7
Giver <ryan01234keroro56789@yahoo.com.tw>
2018-11-26 19:41:21 +0800

    Update Intersection_of_two_lines.cpp

--- geometry/Intersection_of_two_lines.cpp
+++ geometry/Intersection_of_two_lines.cpp
@@ -2,6 +2,6 @@ Pt LLIntersect(Line a, Line b) {
   Pt p1 = a.s, p2 = a.e, q1 = b.s, q2 = b.e;
   double f1 = (p2-p1)^(q1-p1), f2 = (p2-p1)^(p1-q2);
   double f = f1+f2;
-  if(dcmp(f) == 0) return Pt(nan(""), nan(""));
+  if(dcmp(f) == 0) return {nan(""), nan("")};
   return q1*(f2/f) + q2*(f1/f);
 }
