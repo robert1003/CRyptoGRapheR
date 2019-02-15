0f9c6b3e4fdebd4cf44e2d385347cec970452f06
asjndawub <38740582+asjndawub@users.noreply.github.com>
2019-02-15 19:52:07 +0800

    Create Intersection_of_polygon_and_circle 2.cpp

---
+++ geometry/Intersection_of_polygon_and_circle 2.cpp
@@ -0,0 +1,28 @@
+ld PCIntersect(vector<Pt> v, Circle cir) {
+  for(int i = 0 ; i < (int)v.size() ; ++i) v[i] -= cir.o;
+  ld ans = 0, r = cir.r;
+  int n = v.size();
+  for(int i = 0 ; i < n ; ++i) {
+    Pt pa = v[i], pb = v[(i+1)%n];
+    if(norm(pa) < norm(pb)) swap(pa, pb);
+    if(norm(pb) < eps) continue;
+    ld s, h, theta;
+    ld a = norm(pb), b = norm(pa), c = norm(pb-pa);
+    ld cosB = (pb*(pb-pa))/a/c, B = acos(cosB);
+    ld cosC = (pa*pb)/a/b, C = acos(cosC);
+    if(a > r) {
+      s = (C/2)*r*r;
+      h = a*b*sin(C)/c;
+      if(h < r && B < PI/2) S -= (acos(h/r)*r*r - h*sqrt(r*r-h*h));
+    }
+    else if(b > r) {
+      theta = PI - B - asin(sin(B)/r*a);
+      s = 0.5*a*r*sin(theta) + (C-theta)/2*r*r;
+    }
+    else {
+      s = 0.5*sin(C)*a*b;
+    }
+    ans += abs(s)*dcmp(v[i]^v[(i+1)%n]);
+  }
+  return abs(ans);
+}
