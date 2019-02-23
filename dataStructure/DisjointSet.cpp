125015143e9d8ba3c86439003c5b66e350efb4b5
robert1003 <45344470+robert1003@users.noreply.github.com>
2019-02-24 07:53:43 +0800

    Update DisjointSet.cpp

--- dataStructure/DisjointSet.cpp
+++ dataStructure/DisjointSet.cpp
@@ -1,39 +1,46 @@
-struct DisjointSet{
-  // save() is like recursive
-  // undo() is like return
-  int n, fa[ N ], sz[ N ];
-  vector< pair<int*,int> > h;
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DisjointSet {
+  static const int maxn = (int)5e5 + 5;
+  int n, fa[maxn], sz[maxn];
+  vector<pair<int*, int>> h;
   vector<int> sp;
-  void init( int tn ){
-    n=tn;
-    for( int i = 0 ; i < n ; i ++ ){
-      fa[ i ]=i;
-      sz[ i ]=1;
+  
+  void init(int _n) {
+    n = _n;
+    for(int i = 0 ; i < n ; ++i) {
+      fa[i] = i;
+      sz[i] = 1;
     }
     sp.clear(); h.clear();
   }
-  void assign( int *k, int v ){
-    h.PB( {k, *k} );
+  void assign(int *k, int v) {
+    h.push_back({k, *k});
     *k = v;
   }
-  void save(){ sp.PB(SZ(h)); }
-  void undo(){
+  void save() { sp.push_back((int)h.size()); }
+  void undo() {
     assert(!sp.empty());
-    int last=sp.back(); sp.pop_back();
-    while( SZ(h)!=last ){
-      auto x=h.back(); h.pop_back();
+    int last = sp.back(), cnt = 0; sp.pop_back();
+    while(h.size() > last) {
+      auto x = h.back(); h.pop_back();
       *x.first = x.second;
+      cnt++;
     }
+    n += cnt/2;
   }
-  int f( int x ){
-    while( fa[ x ] != x ) x = fa[ x ];
+  int f(int x) {
+    while(fa[x] != x) x = fa[x];
     return x;
   }
-  void uni( int x , int y ){
-    x = f( x ); y = f( y );
-    if( x == y ) return;
-    if( sz[ x ] < sz[ y ] ) swap( x, y );
-    assign( &sz[ x ] , sz[ x ] + sz[ y ] );
-    assign( &fa[ y ] , x);
+  bool merge(int x, int y) {
+    x = f(x); y = f(y);
+    if(x == y) return 0;
+    if(sz[x] < sz[y]) swap(x, y);
+    assign(&sz[x], sz[x]+sz[y]);
+    assign(&fa[y], x);
+    n--;
+    return 1;
   }
-}djs;
+} djs;
