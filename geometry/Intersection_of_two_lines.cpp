5af1cff379ee0d54c3b62b524b4abfc1736a9e4f
devin6011 <38755382+devin6011@users.noreply.github.com>
2019-09-02 20:35:57 +0800

    Fix line intersection local variable type

--- geometry/Intersection_of_two_lines.cpp
+++ geometry/Intersection_of_two_lines.cpp
@@ -1,6 +1,6 @@
 Pt LLIntersect(Line a, Line b) {
   Pt p1 = a.s, p2 = a.e, q1 = b.s, q2 = b.e;
-  double f1 = (p2-p1)^(q1-p1),f2 = (p2-p1)^(p1-q2),f;
+  ld f1 = (p2-p1)^(q1-p1),f2 = (p2-p1)^(p1-q2),f;
   if(dcmp(f=f1+f2) == 0) 
     return dcmp(f1)?Pt(NAN,NAN):Pt(INFINITY,INFINITY);
   return q1*(f2/f) + q2*(f1/f);
