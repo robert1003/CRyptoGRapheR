46789c77c89c10f6a38bb2e79d486b81e9bb4b3e
Giver <ryan01234keroro56789@yahoo.com.tw>
2019-09-12 03:03:22 +0800

    update minimum steiner tree (vertex weight)

--- graph/MinimumSteinerTree.cpp
+++ graph/MinimumSteinerTree.cpp
@@ -1,12 +1,13 @@
-// Minimum Steiner Tree
-// O(V 3^T + V^2 2^T)
+// Minimum Steiner Tree O(V 3^T + V^2 2^T)
+// shortest_path() should be called before solve()
+// w:vertex weight, default 0
 struct SteinerTree{
-#define V 33
-#define T 8
+#define V 66
+#define T 10
 #define INF 1023456789
-  int n , dst[V][V] , dp[1 << T][V] , tdst[V];
+  int n , dst[V][V] , dp[1 << T][V] , tdst[V] , w[V];
   void init( int _n ){
-    n = _n;
+    n = _n; fill( w , w + n , 0 );
     for( int i = 0 ; i < n ; i ++ ){
       for( int j = 0 ; j < n ; j ++ )
         dst[ i ][ j ] = INF;
@@ -18,11 +19,19 @@ struct SteinerTree{
     dst[ vi ][ ui ] = min( dst[ vi ][ ui ] , wi );
   }
   void shortest_path(){
+  	for( int i = 0 ; i < n ; i ++ )
+	  for( int j = 0 ; j < n ; j ++ )
+	  	if( i != j && dst[ i ][ j ] != INF )
+	  	  dst[ i ][ j ] += w[ i ];
     for( int k = 0 ; k < n ; k ++ )
       for( int i = 0 ; i < n ; i ++ )
         for( int j = 0 ; j < n ; j ++ )
           dst[ i ][ j ] = min( dst[ i ][ j ],
                 dst[ i ][ k ] + dst[ k ][ j ] );
+    for( int i = 0 ; i < n ; i ++ )
+	  for( int j = 0 ; j < n ; j ++ )
+	  	if( dst[ i ][ j ] != INF )
+	  	  dst[ i ][ j ] += w[ j ];
   }
   int solve( const vector<int>& ter ){
     int t = (int)ter.size();
@@ -43,12 +52,12 @@ struct SteinerTree{
                  submsk = ( submsk - 1 ) & msk )
             dp[ msk ][ i ] = min( dp[ msk ][ i ],
                             dp[ submsk ][ i ] +
-                            dp[ msk ^ submsk ][ i ] );
+                            dp[ msk ^ submsk ][ i ] - w[ i ] );
       for( int i = 0 ; i < n ; i ++ ){
         tdst[ i ] = INF;
         for( int j = 0 ; j < n ; j ++ )
           tdst[ i ] = min( tdst[ i ],
-                     dp[ msk ][ j ] + dst[ j ][ i ] );
+                     dp[ msk ][ j ] + dst[ j ][ i ] - w[ j ] );
       }
       for( int i = 0 ; i < n ; i ++ )
         dp[ msk ][ i ] = tdst[ i ];
