a9a1946201ee890b7f79d295ae6ff1b4feceb787
Giver <ryan01234keroro56789@yahoo.com.tw>
2019-09-25 15:40:47 +0800

    Update PalTree1.cpp

--- string/PalTree1.cpp
+++ string/PalTree1.cpp
@@ -1,30 +1,30 @@
 const int MXN = 1000010;
 struct PalT{
-  int nxt[MXN][26],fail[MXN],len[MXN],cnt[MXN],num[MXN];
-  int tot,lst,n;
+  int nxt[MXN][26],fail[MXN],len[MXN]; 
+  int tot,lst,n,state[MXN],cnt[MXN],num[MXN];
   char s[MXN]={-1};
   int newNode(int l,int f){
     len[tot]=l,fail[tot]=f,cnt[tot]=num[tot]=0;
     memset(nxt[tot],0,sizeof(nxt[tot]));
     return tot++;
   }
-  int get_fail(int x){
+  int getfail(int x){
     while(s[n-len[x]-1]!=s[n]) x=fail[x];
     return x;
   }
-  void push(){
-    int c=s[n]-'a',np=get_fail(lst);
+  int push(){
+    int c=s[n]-'a',np=getfail(lst);
     if(!(lst=nxt[np][c])){
-      lst=newNode(len[np]+2,nxt[get_fail(fail[np])][c]);
+      lst=newNode(len[np]+2,nxt[getfail(fail[np])][c]);
       nxt[np][c]=lst;
       num[lst]=num[fail[lst]]+1;
     }
-    ++cnt[lst];
+    return ++cnt[lst],lst;
   }
   void init(const char *_s){
     tot=lst=n=0;
     newNode(0,1),newNode(-1,0);
-    for(;_s[n];) s[n+1]=_s[n],++n,push();
+    for(;_s[n];) s[n+1]=_s[n],++n,state[n-1]=push();
     for(int i=tot-1;i>1;i--) cnt[fail[i]]+=cnt[i];
   }
 }palt;
