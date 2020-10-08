fb3e0b9d4c861d3db6431ce93fc304a67e2035c6
Giver <ryan01234keroro56789@yahoo.com.tw>
2020-10-08 16:16:56 +0800

    add custom_hash remove misc

--- basic/misc.cpp
+++ basic/misc.cpp
@@ -3,7 +3,7 @@ mt19937 rng(0x5EED);
 int randint(int lb, int ub)
 { return uniform_int_distribution<int>(lb, ub)(rng); }
 
-#define SECs (clock() / CLOCKS_PER_SEC)
+#define SECs (1.0 * clock() / CLOCKS_PER_SEC)
 
 struct KeyHasher {
 	size_t operator()(const Key& k) const {
