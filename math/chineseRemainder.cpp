17511e7ed6eff4d9b41123a6234212fb7afa42a6
Giver <ryan01234keroro56789@yahoo.com.tw>
2021-01-30 02:43:55 +0800

    Update chineseRemainder.cpp

--- math/chineseRemainder.cpp
+++ math/chineseRemainder.cpp
@@ -1,9 +1,9 @@
-LL solve(LL x1, LL m1, LL x2, LL m2) {
-  LL g = __gcd(m1, m2);
+ll solve(ll x1, ll m1, ll x2, ll m2) {
+  ll g = __gcd(m1, m2);
   if((x2 - x1) % g) return -1;// no sol
   m1 /= g; m2 /= g;
-  pair<LL,LL> p = gcd(m1, m2);
-  LL lcm = m1 * m2 * g;
-  LL res = p.first * (x2 - x1) * m1 + x1;
+  pair<ll,ll> p = gcd(m1, m2);
+  ll lcm = m1 * m2 * g;
+  ll res = p.first * (x2 - x1) * m1 + x1;
   return (res % lcm + lcm) % lcm;
 }
