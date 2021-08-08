8965c5ae8a019d6fd3b1b0f4847aebd1b7a913af
robert1003 <b07902047@ntu.edu.tw>
2021-08-08 10:26:46 +0800

    change zkwflow to dinic_BCW style

--- flow/zkwflow.cpp
+++ flow/zkwflow.cpp
@@ -1,64 +1,71 @@
-struct zkwflow{
-	struct Edge {
-		int to, rev, cap; ll cost;
-	};
-	vector<Edge> g[N];
-	int nv, sv, tv, ptr[N];
-	bool vis[N]; ll dist[N];
-	void init(int n,int s,int t){
-		nv=n+1; sv=s; tv=t;
-		for(int i=0;i<nv;i++) g[i].clear();
+struct zkwflow {
+	static const int maxN = 10000;
+	struct Edge { int v, f, re; ll w; };
+	int n, s, t, ptr[maxN];
+	bool vis[maxN]; ll dis[maxN];
+	vector<Edge> E[maxN];
+
+	void init(int _n, int _s, int _t) {
+		n = _n, s = _s, t = _t;
+		for (int i = 0; i < n; i++) E[i].clear();
 	}
-	void add_edge(int a, int b, int c, ll w) {
-		g[a].push_back(Edge{b,int(g[b].size()),c,w});
-		g[b].push_back(Edge{a,int(g[a].size())-1,0,-w});
+	void add_edge(int u, int v, int f, ll w) {
+		E[u].push_back({v, f, (int)E[v].size(), w});
+		E[v].push_back({u, 0, (int)E[u].size()-1, -w});
 	}
-	bool augment() { // SPFA
-		for (int i = 0; i < nv; i++) {
-			dist[i] = LLINF; vis[i] = false;
-		}
-		dist[sv] = 0;
-		vector<int> que = { sv };
-		for (int i = 0; i < int(que.size()); i++) {
-			int v = que[i];
-			vis[v] = false;
-			for (auto& e : g[v]) {
-				if (e.cap == 0 || dist[e.to] <= dist[v] + e.cost)
-					continue;
-				dist[e.to] = dist[v] + e.cost;
-				if (!vis[e.to]) {
-					vis[e.to] = true;
-					que.push_back(e.to);
+	bool SPFA() {
+		fill_n(dis, n, LLONG_MAX);
+		fill_n(vis, n, false);
+
+		dis[s] = 0;
+		queue<int> q; q.push(s);
+
+		while (!q.empty()) {
+			int u = q.front(); q.pop();
+			vis[u] = false;
+
+			for (auto &it : E[u]) {
+				if (it.f > 0 && dis[it.v] > dis[u] + it.w) {
+					dis[it.v] = dis[u] + it.w;
+					if (!vis[it.v]) {
+						vis[it.v] = true;
+						q.push(it.v);
+					}
 				}
 			}
 		}
-		return dist[tv] != LLINF;
+
+		return dis[t] != LLONG_MAX;
 	}
-	int dfs(int v, int r) {
-		if (v == tv) return r;
-		vis[v] = true;
-		int res = 0;
-		for (int& i = ptr[v]; i < int(g[v].size()); i++) {
-			Edge& e = g[v][i];
-			if (e.cap == 0 || dist[e.to] != dist[v] + e.cost || vis[e.to])
-				continue;
-			int d = dfs(e.to, min(r - res, e.cap));
-			res += d; e.cap -= d;
-			g[e.to][e.rev].cap += d;
-			if (res == r) {
-				vis[v] = false;
-				break;
+	int DFS(int u, int nf) {
+		if (u == t) return nf;
+		int res = 0; vis[u] = true;
+		for (int &i = ptr[u]; i < (int)E[u].size(); i++) {
+			auto &it = E[u][i];
+
+			if (it.f > 0 && dis[it.v] == dis[u] + it.w && !vis[it.v]) {
+				int tf = DFS(it.v, min(nf, it.f));
+				res += tf, nf -= tf, it.f -= tf;
+				E[it.v][it.re].f += tf;
+				if (nf == 0) {
+					vis[u] = false;
+					break;
+				}
 			}
 		}
+
 		return res;
 	}
-	pair<int, ll> solve() {
+	pair<int, ll> flow() {
 		int flow = 0; ll cost = 0;
-		while (augment()) {
-			fill_n(ptr, nv, 0);
-			int d = dfs(sv, INF);
-			flow += d; cost += d * dist[tv];
+
+		while (SPFA()) {
+			fill_n(ptr, n, 0);
+			int f = DFS(s, INT_MAX);
+			flow += f;
+			cost += dis[t] * f;
 		}
+
 		return { flow, cost };
 	}
-}flow;
+} flow;
