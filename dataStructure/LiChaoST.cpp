66a686b9b8797584c3bef6f398d6be670229614e
giver <ryan01234keroro56789@yahoo.com.tw>
2021-11-23 18:58:49 +0800

    improve li chao segment tree

--- dataStructure/LiChaoST.cpp
+++ dataStructure/LiChaoST.cpp
@@ -1,18 +1,24 @@
 struct LiChao_min{
   struct line{
     ll m,c;
-    line(ll _m=0,ll _c=0) { m=_m; c=_c; }
-    ll eval(ll x) { return m*x+c; }
+    line(ll _m=0,ll _c=0){ m=_m; c=_c; }
+    ll eval(ll x){ return m*x+c; } // overflow
   };
   struct node{
     node *l,*r; line f;
-    node(line v) { f=v; l=r=NUll; }
+    node(line v){ f=v; l=r=NULL; }
   };
   typedef node* pnode;
-  pnode root; int sz;
+  pnode root; ll sz,ql,qr;
 #define mid ((l+r)>>1)
-  void insert(line &v,int l,int r,pnode &nd){
-    if(!nd) { nd=new node(v); return; }
+  void insert(line v,ll l,ll r,pnode &nd){
+    /* if(!(ql<=l&&r<=qr)){
+      if(!nd) nd=new node(line(0,INF));
+      if(ql<=mid) insert(v,l,mid,nd->l);
+      if(qr>mid) insert(v,mid+1,r,nd->r);
+      return;
+    } used for adding segment */
+    if(!nd){ nd=new node(v); return; }
     ll trl=nd->f.eval(l),trr=nd->f.eval(r);
     ll vl=v.eval(l),vr=v.eval(r);
     if(trl<=vl&&trr<=vr) return;
@@ -22,17 +28,17 @@ struct LiChao_min{
       insert(v,mid+1,r,nd->r);
     else swap(nd->f,v),insert(v,l,mid,nd->l);
   }
-  ll query(int x,int l,int r,pnode &nd){
-    if(!nd) return LLONG_MAX;
+  ll query(ll x,ll l,ll r,pnode &nd){
+    if(!nd) return INF;
     if(l==r) return nd->f.eval(x);
     if(mid>=x)
       return min(nd->f.eval(x),query(x,l,mid,nd->l));
     return min(nd->f.eval(x),query(x,mid+1,r,nd->r));
   }
   /* -sz<=ll query_x<=sz */
-  void init(int _sz){ sz=_sz+1; root=NUll; }
-  void add_line(ll m,ll c){
-    line v(m,c); insert(v,-sz,sz,root);
+  void init(ll _sz){ sz=_sz+1; root=NULL; }
+  void add_line(ll m,ll c,ll l=-INF,ll r=INF){
+    line v(m,c); ql=l; qr=r; insert(v,-sz,sz,root);
   }
   ll query(ll x) { return query(x,-sz,sz,root); }
 };
