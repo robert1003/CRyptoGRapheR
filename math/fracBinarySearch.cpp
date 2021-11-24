1846e02e9e88e88b17dbcc01ad7f6a39e733aa9a
giver <ryan01234keroro56789@yahoo.com.tw>
2021-11-24 18:02:10 +0800

    add fraction related algorithm into codebook

--- math/fracBinarySearch.cpp
+++ math/fracBinarySearch.cpp
@@ -1,20 +1,19 @@
 //find smallest p/q in [0,1] s.t. f(p/q)=1&&p,q<=N
-struct Frac {ll p, q;};
-Frac fracBS(function<bool(Frac)> f, ll N) {
+struct Frac{ll p,q;};
+Frac fracBS(function<bool(Frac)> f,ll N) {
   bool dir=1,A=1,B=1;
   Frac lo{0,1},hi{1,1}; // set hi to 1/0 to search (0,N]
   if(f(lo)) return lo;
   assert(f(hi));
-  while(A or B) {
+  while(A or B){
     ll adv=0,step=1; // move hi if dir, else lo
-    for(int si=0;step;(step*=2)>>=si) {
-      adv += step;
-      Frac m{lo.p*adv+hi.p,lo.q*adv+hi.q};
+    for(int si=0;step;(step*=2)>>=si){
+      adv+=step; Frac m{lo.p*adv+hi.p,lo.q*adv+hi.q};
       if(abs(m.p)>N or m.q>N or dir==!f(m))
-        adv-=step, si=2;
+        adv-=step,si=2;
     }
-    hi.p += lo.p*adv; hi.q += lo.q*adv;
-    dir = !dir; swap(lo, hi); A=B; B=!!adv;
+    hi.p+=lo.p*adv; hi.q+=lo.q*adv;
+    dir=!dir; swap(lo,hi); A=B; B=!!adv;
   }
-  return dir ? hi : lo;
+  return dir?hi:lo;
 }
