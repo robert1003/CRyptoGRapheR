391cd5c9494add8757904fc17f45dc374d0863bd
devin6011 <t45678h9@yahoo.com.tw>
2021-11-24 15:03:05 +0800

    Fix indentation

--- math/divsumAndModsum.cpp
+++ math/divsumAndModsum.cpp
@@ -1,14 +1,14 @@
 ull sumsq(ull n) {return n/2*((n-1)|1);}
 // sum i=0~n-1 floor((ki+c)/m)
 ull divsum(ull n, ull k, ull c, ull m) {
-    ull res = k/m*sumsq(n)+c/m*n;
-    k%=m; c%=m;
-    if(!k) return res;
-    ull n2 = (n*k+c)/m;
-    return res+(n-1)*n2-divsum(n2,m,m-1-c,k);
+  ull res = k/m*sumsq(n)+c/m*n;
+  k%=m; c%=m;
+  if(!k) return res;
+  ull n2 = (n*k+c)/m;
+  return res+(n-1)*n2-divsum(n2,m,m-1-c,k);
 }
 // sum i=0~n-1 (ki+c)%m
 ll modsum(ull n, ll k, ll c, ll m) {
-    c=(c%m+m)%m; k=(k%m+m)%m;
-    return n*c+k*sumsq(n)-m*divsum(n,k,c,m);
+  c=(c%m+m)%m; k=(k%m+m)%m;
+  return n*c+k*sumsq(n)-m*divsum(n,k,c,m);
 }
