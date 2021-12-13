24d92c23e18254772c3606f3d72770230aac9307
giver <ryan01234keroro56789@yahoo.com.tw>
2021-12-13 15:30:47 +0800

    fix simplex precision

--- math/simplex2.cpp
+++ math/simplex2.cpp
@@ -14,27 +14,27 @@ VDB simplex(int m,int n,vector<VDB> a){
     auto k=a[x][y];a[x][y]=1; vector<int> pos;
     for(int j=0;j<=n;++j){
       a[x][j]/=k;
-      if(a[x][j]!=0) pos.push_back(j);
+      if(dcmp(a[x][j])!=0) pos.push_back(j);
     }
     for(int i=0;i<=m;++i){
-      if(a[i][y]==0||i==x) continue;
+      if(dcmp(a[i][y])==0||i==x) continue;
       k=a[i][y],a[i][y]=0;
       for(int j:pos) a[i][j] -= k*a[x][j];
     }
   };
   for(int x,y;;){
-    for(int i=x=1;i<=m;++i) if(a[i][0]<a[x][0]) x=i;
-    if(a[x][0]>=0) break;
-    for(int j=y=1;j<=n;++j) if(a[x][j]<a[x][y]) y=j;
-    if(a[x][y]>=0) return VDB(); // infeasible
+    for(int i=x=1;i<=m;++i) if(dcmp(a[i][0]-a[x][0])<0) x=i;
+    if(dcmp(a[x][0])>=0) break;
+    for(int j=y=1;j<=n;++j) if(dcmp(a[x][j]-a[x][y])<0) y=j;
+    if(dcmp(a[x][y])>=0) return VDB(); // infeasible
     pivot(x,y);
   }
   for(int x,y;;){
-    for(int j=y=1;j<=n;++j) if(a[0][j]>a[0][y]) y=j;
-    if(a[0][y]<=0) break;
+    for(int j=y=1;j<=n;++j) if(dcmp(a[0][j]-a[0][y])<0) y=j;
+    if(dcmp(a[0][y])<=0) break;
     x=-1;
-    for(int i=1;i<=m;++i) if(a[i][y]>0)
-      if(x==-1||a[i][0]/a[i][y]<a[x][0]/a[x][y]) x=i;
+    for(int i=1;i<=m;++i) if(dcmp(a[i][y])>0)
+      if(x==-1||dcmp(a[i][0]/a[i][y]-a[x][0]/a[x][y])<0) x=i;
     if(x==-1) return VDB(); // unbounded
     pivot(x,y);
   }
